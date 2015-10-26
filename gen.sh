00e3b742b38b134d93be7246c87aa210ac53950b
guzhen <effyroth@gmail.com>
2015-10-27 01:04:43 +0800

    add dubbo idl demo

--- gen.sh
+++
@@ -1,4 +0,0 @@
-thrift --gen java -out src/main/java src/main/thrift/enum.thrift
-thrift --gen java -out src/main/java src/main/thrift/struct.thrift
-thrift --gen java -out src/main/java src/main/thrift/exception.thrift
-thrift --gen java -out src/main/java src/main/thrift/service.thrift
